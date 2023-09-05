f71c62f02a40824c1c9041fddab49a8820c3bba2
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-05 09:40:21 -0700

    8. To uppercase tasks completed

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(str):
+    result_str = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            result_str += chr(ord(str[i]) - 32)
+            continue
+        result_str += str[i]
+
+    print('{}'.format(result_str))
