39fc093a07ff8e7515337e1fc10a507940cb277d
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-07 07:36:32 -0700

    8. ByteCode -> Python #3 tasks completed

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
     if (a < b):
         c = add(a, b)
-        for func in range(4, 6):
-            c = add(c, func)
-            return (c)
+    for func in range(4, 6):
+        c = add(c, func)
+        return (c)
     else:
         return (sub(a, b))
