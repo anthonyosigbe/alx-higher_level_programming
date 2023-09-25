8e00a791a89185d942032ca486a08a8f8dbb875b
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-25 04:13:10 -0700

    3. Integers division with debug tasks completed

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        quotient = a / b
+    except (TypeError, ZeroDivisionError):
+        quotient = None
+    finally:
+        print("Inside result: {}".format(quotient))
+    return (quotient)
