fb77cf63ee7d4724137ba85421ee7098f34358ff
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-26 05:55:52 -0700

    5. Printing a square tasks completed

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Create a Square class."""
+
+
+class Square:
+    """Depict a square."""
+    def __init__(self, size=0):
+        """Instantiate a fresh Square.
+
+        Args:
+            size (int): The dimensions of the newly created square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve or modify the current square size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Provide the current square's area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Display the square using the '#' character."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
