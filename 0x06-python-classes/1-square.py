b4a1a304fa761ddad2d56c96d392345bbaf7044d
Anthony Osigbe <thronesofglory@gmail.com>
2023-09-26 04:07:07 -0700

    1. Square with size tasks completed

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Create a Square class."""
+
+
+class Square:
+    """Illustrate a square."""
+    def __init__(self, size):
+        """Create a new instance of the Square class.
+
+        Args:
+            size (int): Integer value representing,
+            the size of the newly created square.
+        """
+        self.__size = size
