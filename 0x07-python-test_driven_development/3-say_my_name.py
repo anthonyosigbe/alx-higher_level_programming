c1fccada692636248e7edcb316a0da08c2b684e7
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-02 07:41:37 -0700

    2. Say my name tasks completed

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Establishes a function for printing names."""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints a formatted string with the first name and last name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
