74678b66312708ad9de34c616595c9ca87d522e3
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-02 07:44:14 -0700

    4. Text indentation tasks completed

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Establishes a function for indenting text."""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each '.', '?',
+    and ':' characters.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
