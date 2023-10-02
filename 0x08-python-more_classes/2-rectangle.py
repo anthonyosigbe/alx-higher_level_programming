f10964efced4a5c6d4770244e227a03c3a3fc4b1
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-02 01:11:19 -0700

    2. Area and Perimeter tasks completed

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Establishes a class named Rectangle."""
+
+
+class Rectangle:
+    """Depict a rectangle."""
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle (default is 0).
+            height (int): The height of the rectangle (default is 0).
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve or modify the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve or adjust the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Provide the area of the Rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
