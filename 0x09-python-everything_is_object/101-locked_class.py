e5e57de608ba1cce35c05846fe8b87d6c867bd53
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-03 07:46:15 -0700

    30. Low memory cost tasks completed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Specifies a class that is locked."""
+
+
 class LockedClass(object):
+    """Restrict the creation of new attributes in LockedClass,
+    to only those named 'first_name' for users.
+    """
     __slots__ = 'first_name'
