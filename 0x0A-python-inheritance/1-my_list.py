9f3d5765db30ee23c2493ed0784e382a3532aee2
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-09 05:24:34 -0700

    bug fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,4 @@ class MyList(list):
     """Introduces sorted printing functionality for the standard list class."""
     def print_sorted(self):
         """Display a list in ascending sorted order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
