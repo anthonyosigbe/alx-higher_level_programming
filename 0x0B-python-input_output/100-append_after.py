feabf04370190e3512bc0cb37182dc259f610904
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-10 06:55:38 -0700

    13. Search and update tasks completed

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Declares a function for inserting text into a file."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Add text after each line that contains a specified string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
