e893286ef73dcc9954c243483246f78ba74b05e9
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-14 15:08:59 -0700

    15. Dictionary to JSON string tasks completed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Provide the JSON serialization of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list): An array of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+
+        return json.dumps(list_dictionaries)
