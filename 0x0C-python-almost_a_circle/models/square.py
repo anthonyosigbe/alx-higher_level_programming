c80472b6e3e4434d6c4b8672daf3a3bf781c1e11
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-14 13:17:56 -0700

    13. Rectangle instance to dictionary representation tasks completed

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,44 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Modify the Square.
+
+        Args:
+            *args (ints): Fresh attribute values.
+                - The first argument represents the 'id' attribute.
+                - The second argument corresponds to the 'size' attribute.
+                - The third argument represents the 'x' attribute.
+                - The fourth argument corresponds to the 'y' attribute.
+            **kwargs (dict): Fresh sets of key/value attribute pairs.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
