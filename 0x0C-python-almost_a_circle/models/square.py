59848b75122c1eab273806c4fc92c7404be3dfaa
Anthony Osigbe <thronesofglory@gmail.com>
2023-10-14 11:54:41 -0700

    10. And now, the Square! tasks completed

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Specifies a class for squares."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Depict a square."""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Create a new instance of a Square.
+
+        Args:
+            size (int): The size of the newly created Square.
+            x (int): The horizontal coordinate (x) of the newly,
+            created Square.
+            y (int): The vertical coordinate (y) of the newly,
+            created Square.
+            id (int): The unique identifier of the newly created Square.
+        """
+        super().__init__(size, size, x, y, id)
