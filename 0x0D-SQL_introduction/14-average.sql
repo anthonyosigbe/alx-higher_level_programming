5808a4614b1f577b222aecbb4f90c6b51f03d682
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-14 13:34:12 -0800

    14. Average tasks completed

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Calculates the average value of the `score` for all records in
+-- the table `second_table` of the database `hbtn_0c_0`
+-- in MySQL Server.
+SELECT AVG(`score`) as `average`
+FROM second_table;
