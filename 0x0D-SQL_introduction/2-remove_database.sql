8634781eac207071fff9269e3fd262562dfbff85
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-14 12:24:32 -0800

    2. Delete a database tasks completed

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes the database `hbtn_0c_0` in MySQL Server.
+-- If the `hbtn_0c_0` database does not exist, the script should not encounter any failures.
+DROP DATABASE IF EXISTS hbtn_0c_0;
