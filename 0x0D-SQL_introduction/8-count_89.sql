75ecd23d144a376b455d2f24b0c27022e11d08ae
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-14 13:15:11 -0800

    8. Count 89 tasks completed

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Shows the count of records where `id` is equal to '89'.
+-- in the table `first_table` of the database
+-- `hbtn_0c_0` in MySQL Server.
+SELECT COUNT(id) FROM first_table WHERE `id` = '89';
