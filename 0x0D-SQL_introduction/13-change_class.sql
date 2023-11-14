b97c761e6c8171f425d600755c3be41b62a5162f
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-14 13:31:43 -0800

    13. Score too low tasks completed

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- Deletes all records with a `score` less than or equal to 5 in
+-- the table `second_table` of the database `hbtn_0c_0`
+-- in MySQL Server.
+DELETE FROM second_table
+WHERE `score` <= 5;
