8bd0cead5d6002e33ae84fda7858d4560b915598
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-15 06:40:05 -0800

    19. Rotten tomatoes tasks completed

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,8 @@
 -- Displays all genres in the database hbtn_0d_tvshows_rate by their rating.
 -- Records are ordered by descending rating.
 
-SELECT `name`, SUM(`rate`) AS `rating`
-FROM `tv_genres` AS g
-INNER JOIN `tv_show_genres` AS s ON s.`genre_id` = g.`id`
-INNER JOIN `tv_show_ratings` AS r ON r.`show_id` = s.`show_id`
-GROUP BY `name`
-ORDER BY `rating` DESC;
+SELECT s.title, IFNULL(SUM(r.rate), 0) AS rating
+FROM tv_shows s
+LEFT JOIN tv_show_ratings r ON s.id = r.show_id
+GROUP BY s.title
+ORDER BY rating DESC;
