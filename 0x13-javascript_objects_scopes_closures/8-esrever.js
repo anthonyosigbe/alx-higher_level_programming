aab8b3b4e2cda47257ada48ccec0aa120f5f9b8a
Anthony Osigbe <thronesofglory@gmail.com>
2023-12-12 05:27:45 -0800

    8. Esrever tasks completed

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* function that returns the reversed version of a list. */
+exports.esrever = function (list) {
+  const reversedArray = [];
+
+  for (let i = list.length - 1; i >= 0; --i) {
+    reversedArray.push(list[i]);
+  }
+
+  return reversedArray;
+};
