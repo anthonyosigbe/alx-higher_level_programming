4549c63bf805741b58d1877fb889dde03c75263b
Anthony Osigbe <thronesofglory@gmail.com>
2023-12-12 02:50:27 -0800

    1. Rectangle #1 tasks completed

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Specifies a class named Rectangle with attributes for width and height. */
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
