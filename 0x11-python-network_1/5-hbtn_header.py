d959d7e746b8cd18d1ac360b9c4c0cb6241cdc97
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-06 21:07:20 -0800

    5. Response header value #1 tasks completed

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL
+and displays the value of the `X-Request-Id`
+variable found in the header of the response.
+
+Usage: ./5-hbtn_header.py <URL>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
