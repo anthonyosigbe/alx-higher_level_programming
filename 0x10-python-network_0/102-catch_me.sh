3dd4d21c81bc3de585b5dd3ed7b585fc05a2c042
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-13 05:47:26 -0800

    project task update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:You got me!"
