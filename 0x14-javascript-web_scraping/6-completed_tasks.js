ec396cbeec20f4a8021a66b992315977136388f6
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-30 03:13:09 -0800

    6. How many completed? tasks completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let index = 0; index < body.length; ++index) {
+      const userId = body[index].userId;
+      const completed = body[index].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
+  }
+});
