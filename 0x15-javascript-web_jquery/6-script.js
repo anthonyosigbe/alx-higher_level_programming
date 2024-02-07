c235ce6240c80c3ac36d55b135753547a29c233d
Anthony Osigbe <thronesofglory@gmail.com>
2024-02-07 03:16:18 -0800

    6. Change the text tasks completed

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+// Attach a click event listener to DIV#update_header using jQuery
+
+$('#update_header').click(function () {
+  // Update the text of the <header> element
+  $('header').text('New Header!!!');
+});
