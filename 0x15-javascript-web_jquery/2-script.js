55802b73d152ccc5253a622382e67ecfb2ff5c87
Anthony Osigbe <thronesofglory@gmail.com>
2024-02-07 02:45:44 -0800

    2. Click and turn red tasks completed

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 // Use the jQuery API to change the color of the `div#red_header` to red when clicked.
 
-$('div#red_header').click(function () {
+$('#red_header').click(function () {
   $('header').css('color', '#FF0000');
 });
