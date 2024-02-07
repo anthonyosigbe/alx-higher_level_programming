9b43bafe0537cbd2b5763bf0d4a3040bc3f40b26
Anthony Osigbe <thronesofglory@gmail.com>
2024-02-07 03:46:48 -0800

    10. No jQuery - document loaded tasks completed

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// Listen for the `DOMContentLoaded` event to ensure the DOM is fully loaded.
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Utilize `document.querySelector` to select the `<header>`
+  // element and modify its color.
+  document.querySelector('header').style.color = '#FF0000';
+});
